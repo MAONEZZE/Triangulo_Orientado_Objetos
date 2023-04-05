191825b32df97b7bbb8732fa5ce45c0034f6506a
ruan <ruansanchezskt@gmail.com>
2023-04-05 19:04:44 -0300

    Exercício do Triangulo Orientado Objetos

---
+++ ExTrianguloPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ExTrianguloPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ladoA, ladoB, ladoC;
+            string nomeTriangulo;
+
+            Console.Write("Digite o lado A do triangulo: ");
+            ladoA = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o lado B do triangulo: ");
+            ladoB = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o lado C do triangulo: ");
+            ladoC = Convert.ToDouble(Console.ReadLine());
+
+            Triangulo tri = new Triangulo(ladoA, ladoB, ladoC);
+
+            nomeTriangulo = tri.verificaLado();
+
+            Console.WriteLine(nomeTriangulo);
+
+        }
+    }
+}
---
+++ ExTrianguloPOO/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ExTrianguloPOO
+{
+    internal class Triangulo
+    {
+        public double ladoA, ladoB, ladoC;
+
+        public Triangulo(double ladoA, double ladoB, double ladoC)
+        {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB; 
+            this.ladoC = ladoC;
+        }
+
+        public string verificaLado()
+        {
+            if (this.ladoA == this.ladoB && this.ladoA == this.ladoC)
+            {
+                return "Triângulo Equilátero";
+            } 
+            else {
+                if (this.ladoA == this.ladoB || this.ladoA == this.ladoC || this.ladoB == this.ladoC)
+                {
+                   return "Triângulo Isósceles";
+                }
+                else
+                {
+                    return "Triângulo Escaleno";
+                }
+
+            }
+        }
+    }
+}
